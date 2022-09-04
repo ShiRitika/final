a5d3f6d66c99560c7f9c65b8b8819e980b3322fb
ShiRitika <shiritika15031999@gmail.com>
2022-09-04 10:30:19 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//how to use this keyword in method
+
+let person = {
+    firstName : "Ritika",
+    lastName : "Tiwari",
+    age : 24,
+    function(){
+        console.log("hello i am "+ this.firstName +" and i have a "+ cars.car1 + " car.");
+    } 
+};
+let cars ={
+    car1 : "Tata",
+    car2 : "BWD"
+};
+person.function();
